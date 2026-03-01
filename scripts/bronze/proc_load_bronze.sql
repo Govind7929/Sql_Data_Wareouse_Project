746db0c3bd3f4d989bf6d0707e70118ba6d39876
Govind7929 <93856873+Govind7929@users.noreply.github.com>
2026-03-01 15:49:57 +0530

    Create procedure to load data into bronze tables

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,75 @@
+
+
+
+CREATE OR ALTER PROCEDURE bronze.load
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    TRUNCATE TABLE bronze.crm_cust_info;
+
+    BULK INSERT bronze.crm_cust_info
+    FROM 'C:\Users\govind\Downloads\sql datasets\sql warehouse project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '\n',
+        TABLOCK
+    );
+
+    TRUNCATE TABLE bronze.crm_prd_info;
+
+    BULK INSERT bronze.crm_prd_info
+    FROM 'C:\Users\govind\Downloads\sql datasets\sql warehouse project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '\n',
+        TABLOCK
+    );
+
+    TRUNCATE TABLE bronze.crm_sales_details;
+
+    BULK INSERT bronze.crm_sales_details
+    FROM 'C:\Users\govind\Downloads\sql datasets\sql warehouse project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '\n',
+        TABLOCK
+    );
+
+    TRUNCATE TABLE bronze.erp_loc_a101;
+
+    BULK INSERT bronze.erp_loc_a101
+    FROM 'C:\Users\govind\Downloads\sql datasets\sql warehouse project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '\n',
+        TABLOCK
+    );
+
+    TRUNCATE TABLE bronze.erp_cust_az12;
+
+    BULK INSERT bronze.erp_cust_az12
+    FROM 'C:\Users\govind\Downloads\sql datasets\sql warehouse project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '\n',
+        TABLOCK
+    );
+
+    TRUNCATE TABLE bronze.erp_px_cat_giv2;
+
+    BULK INSERT bronze.erp_px_cat_giv2
+    FROM 'C:\Users\govind\Downloads\sql datasets\sql warehouse project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '\n',
+        TABLOCK
+    );
+
+END;
